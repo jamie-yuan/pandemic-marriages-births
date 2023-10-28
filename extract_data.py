0a66b372ebeacfcf22ccd183ce35f8fda80f4aa8
jamie-yuan <jamie.q.yuan@gmail.com>
2023-10-28 15:28:13 -0400

    extract more data from earlier years

--- extract_data.py
+++ extract_data.py
@@ -1,12 +1,20 @@
 """CSC110 Fall 2021 Project: Extracting the Data
 
-This Python module contains the functions that pull the relevant COVID-19, marriage, and birth data
-from the COVID19 Data and Vital Events Data by Month datasets using Pandas. The data is formatted
-and added to separate dictionaries.
+This Python module contains the functions that extract the relevant COVID-19, marriage, and birth
+data from the COVID19 Data and Vital Events Data by Month datasets using Pandas. The data is
+formatted and added to separate dictionaries.
 
 This file is Copyright (c) 2021 Cosette Li, Amelia Riddell, and Jamie Yuan.
 """
 import pandas as pd
+
+COVID_REGION = 1
+COVID_DATE = 3
+COVID_NUM_CONFIRMED_CASES = 5
+EVENTS_MONTH = 0
+EVENTS_YEAR = 1
+EVENTS_NUM_BIRTHS = 2
+EVENTS_NUM_MARRIAGES = 3
 
 
 class MonthNotFoundError(Exception):
@@ -17,95 +25,102 @@ class MonthNotFoundError(Exception):
     def __str__(self) -> str:
         """Return a string representation of this error.
         """
-        return 'No month was found in the string.'
+        return "No month was found in the string."
 
 
-def covid19_data() -> dict[str: int]:
-    """Read and add relevant data from COVID19 Data to a dictionary mapping
-    month and year of cases (2018-2020) to its corresponding number of cases, using Pandas.
+def convert_month_to_num(month_name: str) -> int:
+    """Convert a string containing a full month name to the positive integer n where the month is
+    the nth month of the year.
 
     Preconditions:
-        - The file 'covid19_data.csv' is in the same directory as this file.
+        - any(month in month_name for month in ["January", "February", "March", "April", "May",
+        "June", "July", "August", "September", "October", "November", "December"])
+
+    >>> convert_month_to_num("January/janvier") == 1
+    True
+    >>> convert_month_to_num("August/août") == 8
+    True
     """
-    covid_file = 'covid19_data.csv'
+    months = ["January", "February", "March", "April", "May", "June", "July", "August",
+              "September", "October", "November", "December"]
+
+    for i in range(len(months)):
+        if months[i] in month_name:
+            return i + 1
+
+    raise MonthNotFoundError
+
+
+def extract_covid19_data() -> dict[tuple: int]:
+    """Read and add relevant data from the COVID19 Data dataset to a dictionary mapping month and
+    year of cases (January 2020 - June 2021) to the corresponding number of confirmed cases,
+    using Pandas.
+
+    Preconditions:
+        - The file "covid19_data.csv" is in the data directory.
+    """
+    covid_file = "data/covid19_data.csv"
     read_file = pd.read_csv(covid_file)
     cases_by_month = {}
 
     for row in read_file.iterrows():
-        date = row[1][3][0:7]
-        if (row[1][1] == 'Canada') and ('2020' in date):
+        if "2021-07" in row[1][COVID_DATE]:
+            break
+
+        if row[1][COVID_REGION] == "Canada":
+            date = row[1][COVID_DATE]  # format: YYYY-MM-DD
+            date = (int(date[5:7]), int(date[0:4]))  # format: (month, year)
             if date not in cases_by_month:
                 cases_by_month[date] = 0
-            cases_by_month[date] += row[1][5]
+            cases_by_month[date] += row[1][COVID_NUM_CONFIRMED_CASES]  # number of confirmed cases
 
     return cases_by_month
 
 
-def marriage_data() -> dict[str, int]:
-    """Read and add relevant marriage data from Vital Events Data by Month to a dictionary mapping
-    month and year of marriages (2018-2020) to its corresponding number of marriages, using Pandas.
+def extract_marriage_data() -> dict[tuple, int]:
+    """Read and add relevant marriage data from the Vital Events Data by Month dataset to a
+    dictionary mapping month and year of marriages (January 2010 - June 2021) to the corresponding
+    number of marriages, using Pandas.
 
     Preconditions:
-        - The file 'vital_events_data_by_month.csv' is in the same directory as this file.
+        - The file "vital_events_data_by_month.csv" is in the data directory.
     """
-    marriages_file = 'vital_events_data_by_month.csv'
+    marriages_file = "data/vital_events_data_by_month.csv"
     read_file = pd.read_csv(marriages_file)
     marriages_by_month = {}
+    relevant_years = set(range(2010, 2022))
 
     for row in read_file.iterrows():
-        year = str(row[1][1])
-        if year in {'2018', '2019', '2020'}:
-            month = month_to_number(row[1][0])
-            year_month = year + '-' + month
-            marriages_by_month[year_month] = row[1][3]
+        year = int(row[1][EVENTS_YEAR])
+        if year in relevant_years:
+            month = convert_month_to_num(row[1][EVENTS_MONTH])
+            date = (month, year)
+            marriages_by_month[date] = row[1][EVENTS_NUM_MARRIAGES]
 
     return marriages_by_month
 
 
-def birth_data() -> dict[str, int]:
-    """Read and add relevant birth data from Vital Events Data by Month to a dictionary mapping
-    month and year of births (2018-2020) to its corresponding number of live births, using Pandas.
+def extract_birth_data() -> dict[tuple, int]:
+    """Read and add relevant birth data from the Vital Events Data by Month dataset to a
+    dictionary mapping month and year of births (January 2010 - June 2021) to the corresponding
+    number of live births, using Pandas.
 
     Preconditions:
-        - The file 'vital_events_data_by_month.csv' is in the same directory as this file.
+        - The file "vital_events_data_by_month.csv" is in the data directory.
     """
-    births_file = 'vital_events_data_by_month.csv'
+    births_file = "data/vital_events_data_by_month.csv"
     read_file = pd.read_csv(births_file)
     births_by_month = {}
+    relevant_years = set(range(2010, 2022))
 
     for row in read_file.iterrows():
-        year = str(row[1][1])
-        if year in {'2018', '2019', '2020'}:
-            month = month_to_number(row[1][0])
-            year_month = year + '-' + month
-            births_by_month[year_month] = row[1][2]
+        year = int(row[1][EVENTS_YEAR])
+        if year in relevant_years:
+            month = convert_month_to_num(row[1][EVENTS_MONTH])
+            date = (month, year)
+            births_by_month[date] = row[1][EVENTS_NUM_BIRTHS]
 
     return births_by_month
-
-
-def month_to_number(full_month_name: str) -> str:
-    """Convert a full month name to a string representation of the number n where the month is
-    the nth month of the year.
-
-    Preconditions:
-        - any(month in full_month_name for month in ['January', 'February', 'March', 'April', 'May',
-        'June', 'July', 'August', 'September', 'October', 'November', 'December'])
-
-    >>> month_to_number('January/janvier') == '01'
-    True
-    >>> month_to_number('Month of birth, December') == '12'
-    True
-    """
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-              'September', 'October', 'November', 'December']
-
-    for i in range(len(months)):
-        if months[i] in full_month_name and i + 1 < 10:
-            return '0' + str(i + 1)
-        elif months[i] in full_month_name and i + 1 >= 10:
-            return str(i + 1)
-
-    raise MonthNotFoundError
 
 
 # if __name__ == '__main__':
